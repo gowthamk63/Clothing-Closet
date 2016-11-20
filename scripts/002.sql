8ba944f60a091beea0b49e306cd42130fb16491a
naveendennis <naveendennis@gmail.com>
2016-11-20 17:35:33 -0500

    Added script files

--- scripts/002.sql
+++ scripts/002.sql
@@ -4,3 +4,5 @@ alter table item modify id int(20) auto_increment;
 alter table donator add constraint foreign key (id) references person(id);
 alter table donation_history add constraint foreign key (personid) references donator(id);
 alter table purchase_history add constraint foreign key (personid) references buyer(id);
+alter table role modify column id int(20) auto_increment;
+
