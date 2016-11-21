53efdb7228606d49f60ee605478302b1aa487195
naveendennis <naveendennis@gmail.com>
2016-11-21 11:01:48 -0500

    changes to the script file

--- util/connect.php
+++ util/connect.php
@@ -1,10 +1,11 @@
 <?php
 include 'loadConfiguration.php';
+
 $host = $config ['database'] ['host'];
 $username = $config ['database'] ['user'];
 $password = $config ['database'] ['password'];
 $db_name = $config ['database'] ['dbname'];
 
 // Connect to server and select databse.
-$con = new mysqli ( "$host", "$username", "$password", "$db_name" );
+$con = new mysqli ( "$host", "$username", "$password", "$db_name" ) or die('Connection Error');
 ?>
