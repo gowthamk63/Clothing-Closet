b4d63daafc25fe64f673299ec2dfb2426b79da45
gowtham <gowthamk63@gmail.com>
2016-11-23 12:26:30 -0500

    temp

--- login.php
+++ login.php
@@ -3,26 +3,24 @@ include 'util/console_logger.php';
 if (isset ( $_POST ['btn-login'] )) {
 	require 'util/connect.php';
 	$tbl_name = "login"; // Table name
-	                   
+
 	// and password sent from form
 	$email = $_POST ['email'];
 	$password = $_POST ['password'];
-	console_log ( $email );
-	console_log ( $password );
-	
+
 	// To protect MySQL injection (more detail about MySQL injection)
 	// $email = stripslashes($email);
 	// $password = stripslashes($password);
 	// $email = mysqli_real_escape_string($email);
 	// $password = mysqli_real_escape_string($password);
-	
+
 	$sql = "select p.id, p.email, l.password from login l join person p on p.id=l.personid WHERE p.email='$email' and l.password='$password';";
-	console_log ( $sql );
+	
 	$result = $con->query ( $sql ) or die ( $con->connect_error );
 	// counting table rows
 	$count = $result->num_rows;
 	$row = $result->fetch_array ();
-	
+
 	// If result matched $email and $password, table row must be 1 row
 	if ($count == 1 && $row ['password'] == $password) {
 		session_start ();
--- util/loadConfiguration.php
+++ util/loadConfiguration.php
@@ -3,5 +3,5 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
-$config = parse_ini_file('../resources/dbconfiguration.ini',true) or die('Error');
+$config = parse_ini_file('resources/dbconfiguration.ini',true) or die('Error');
 ?>
